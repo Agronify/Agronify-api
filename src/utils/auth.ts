116bc73c1332104ae5423725fe4466971e5f0830
Muhammad Eko Prasetyo <muhammadekoprasetyo29@gmail.com>
2023-05-21 13:50:45 +0700

    move generatetoken to utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,20 @@
+import { User } from "@prisma/client";
+import jwt from "jsonwebtoken";
+export default class AuthUtils {
+  public static async generateToken(user: User, expiresIn = "1h") {
+    return jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        phone: user.phone,
+        name: user.name,
+        is_admin: user.is_admin,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1h",
+        algorithm: "HS256",
+      }
+    );
+  }
+}
